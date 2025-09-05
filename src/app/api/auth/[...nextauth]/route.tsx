c1940aa618fcce19e062a2cec534aea633098bcf
Vibe Shared <vibeshared@gmail.com>
2025-09-05 16:52:19 +0530

    add facebook nextauth login

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 import mongoose from "mongoose";
 import connectdb from "@/lib/Connect";
 import User from "@/lib/models/User";
@@ -9,7 +10,11 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    })
+    }),
+    FacebookProvider({
+    clientId: process.env.FACEBOOK_CLIENT_ID !,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET ! ,
+  })
   ],
   callbacks: {
     // ✅ When user signs in
